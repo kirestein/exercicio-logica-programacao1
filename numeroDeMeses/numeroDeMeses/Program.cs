bc35ffd3332d410437b11676a0f587fb904cd47e
= <=>
2021-06-17 11:42:21 -0300

    Calculando o número de dias e x meses

---
+++ numeroDeMeses/numeroDeMeses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace numeroDeMeses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalDeDias = 0, diasDoMes = 30, n = 0;
+            
+
+            do
+            {
+                Console.WriteLine("Digite o número de meses que deseja calcular a quantidade de dias: ");
+                string numerosDeMeses = Console.ReadLine();
+                n = Convert.ToInt32(numerosDeMeses);
+                totalDeDias = diasDoMes * n;
+
+                Console.WriteLine("O número de dias de " + n + " meses é: " + totalDeDias);
+
+            } while (n > 0);
+    }
+    }
+}
